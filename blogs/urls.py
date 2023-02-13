e7a81502950e8eae3e0aac7c1410b6cd8cd7fd6f
ashura <ghanatva@gmail.com>
2023-02-13 19:14:41 +0530

    "gha"

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
 from . import views
 
 urlpatterns=[
-    path('',views.index,name="index"),
-    path('about',views.about,name="about")
+    path('',PostListView.as_view(),name="index"),
+    path('about',views.about,name="about"),
+    path('post/<int:pk>/',PostDetailView.as_view(),name="post_detail"),
+    path('post/new',PostCreateView.as_view(),name="post_create"),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name="post_update"),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name="post_delete"),
 ]
