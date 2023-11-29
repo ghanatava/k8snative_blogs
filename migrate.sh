61ea292a9a82ca9bf865f055c94cb3d2da29535c
ghanatava <ghanatva@gmail.com>
2023-11-29 14:03:33 +0530

    automated super user creation

--- migrate.sh
+++ migrate.sh
@@ -6,4 +6,4 @@ SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@admin.com"}
 cd /app/
 
 python manage.py migrate --noinput
-python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+(echo "from django.contrib.auth.models import User; User.objects.create_superuser($DJANGO_SUPERUSER_NAME,$SUPERUSER_EMAIL,$DJANGO_SUPERUSER_PASSWORD)" | python3 manage.py shell) || true
